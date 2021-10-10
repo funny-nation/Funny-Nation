40465ee3e4eb02708ce90dea44552ba91493d235
plbin97 <i@teenet.me>
2021-10-10 12:02:59 -0400

    Fix bug on migration

--- migrations/20211006_01_gSl3C-create-user-database.py
+++ migrations/20211006_01_gSl3C-create-user-database.py
@@ -7,5 +7,5 @@ from yoyo import step
 __depends__ = {}
 
 steps = [
-    step("CREATE TABLE `funnyNationsDevelopment`.`user` ( `userID` BIGINT(255) NOT NULL , `money` BIGINT(255) NOT NULL , `lastEarnFromMessage` TIMESTAMP NOT NULL , `lastCheckIn` TIMESTAMP NOT NULL , PRIMARY KEY (`userID`)) ENGINE = InnoDB;")
+    step("CREATE TABLE `user` ( `userID` BIGINT(255) NOT NULL , `money` BIGINT(255) NOT NULL , `lastEarnFromMessage` TIMESTAMP NOT NULL , `lastCheckIn` TIMESTAMP NOT NULL , PRIMARY KEY (`userID`)) ENGINE = InnoDB;")
 ]
--- migrations/20211006_02_Zsic7-create-cashflow-table.py
+++ migrations/20211006_02_Zsic7-create-cashflow-table.py
@@ -7,5 +7,5 @@ from yoyo import step
 __depends__ = {'20211006_01_gSl3C-create-user-database'}
 
 steps = [
-    step("CREATE TABLE `funnyNationsDevelopment`.`cashFlow` ( `flowID` BIGINT(255) NOT NULL AUTO_INCREMENT , `userID` BIGINT(255) NOT NULL , `amount` BIGINT(255) NOT NULL , `message` TEXT NOT NULL , `date` TIMESTAMP NOT NULL , PRIMARY KEY (`flowID`)) ENGINE = InnoDB;")
+    step("CREATE TABLE `cashFlow` ( `flowID` BIGINT(255) NOT NULL AUTO_INCREMENT , `userID` BIGINT(255) NOT NULL , `amount` BIGINT(255) NOT NULL , `message` TEXT NOT NULL , `date` TIMESTAMP NOT NULL , PRIMARY KEY (`flowID`)) ENGINE = InnoDB;")
 ]
