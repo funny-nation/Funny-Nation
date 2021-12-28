709ca88c900788d0b5220067961aa824499069d2
plbin97 <i@teenet.me>
2021-12-27 19:24:30 -0500

    new docker file

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import configparser
 from src.Robot import Robot
-
+from src.checkBeforeRun.checkBeforeRun import checkBeforeRun
 
 def run():
-
+    checkBeforeRun()
     config = configparser.ConfigParser()
     config.read('config.ini', encoding='utf-8')
-
     robot = Robot()
     robot.run(config['private']['token'])
 
