a1d2761f1fbce9ca1b8aba4f7d55c367715a4d5b
plbin97 <i@teenet.me>
2022-01-02 11:05:14 -0500

    log the roles fetched in VIP feature

--- src/utils/getVipRoles.py
+++ src/utils/getVipRoles.py
@@ -1,6 +1,7 @@
 from discord import Guild, Role
 from src.readConfig import getVipTagsConfig
 from typing import List
+from loguru import logger
 
 async def getVipRoles(myGuild: Guild):
     vipTagsConfig = getVipTagsConfig()
@@ -16,5 +17,6 @@ async def getVipRoles(myGuild: Guild):
         for i in vipLevelsFromConfig:
             if vipTagsConfig[i]['id'] == str(role.id):
                 vipRoles[int(i)] = role
+                logger.info(f"Fetched role of {vipTagsConfig[i]['name']}")
 
     return vipRoles
