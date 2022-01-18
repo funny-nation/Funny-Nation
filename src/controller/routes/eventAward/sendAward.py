168c64c479d155a1e3aaa42cb1bb02b46aec409d
Yinliang Lyu <ylyu2@binghamton.edu>
2022-01-17 19:53:44 -0500

    working on management file

--- src/controller/routes/eventAward/sendAward.py
+++ src/controller/routes/eventAward/sendAward.py
@@ -1,6 +1,6 @@
 from discord import Client, TextChannel, Guild, Member, Message
 from pymysql import Connection
-from src.model.eventAwardManagement import deletAward, newAward
+from src.model.eventAwardManagement import deletAward, newAward, editRecipient, takeAward, getEventAward
 from src.model.userManagement import getUser, addMoneyToUser
 from src.model.cashFlowManagement import addNewCashFlow
 from src.model.luckyMoneyManagement import newLuckyMoney
@@ -12,6 +12,10 @@ def sendAward(self: Client, message: Message, db: Connection, money: int, userID
     uuid = newAward()
     myGuild: Guild = self.guilds[0]
     user: Member = await myGuild.fetch_member(userID)
+
+    if newAward() is None:
+        deletAward()
+        return
 
     if deletAward(db, uuid):
         msg = languageConfig['eventAward']['closeEvent'] \
