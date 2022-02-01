f5c253d05bd47fef1f115c700da7c81adf1a1f6e
Yinliang Lyu <ylyu2@binghamton.edu>
2022-02-01 13:25:31 -0500

    new config

--- src/controller/publicMsgRouter.py
+++ src/controller/publicMsgRouter.py
@@ -72,9 +72,6 @@ async def publicMsgRouter(self: Client, message: Message, db: Connection, storag
     if re.match(f"^管理员加钱 [0-9]+\.?[0-9]* \<\@\!?[0-9]+\>$", command):
         await addMoneyAdmin(self, db, message, command, storage.admins)
         return
-
-    if re.match(f"^礼物 (.+) [1-9][0-9]* \<\@\!?[0-9]+\>$", command):
-    
     if re.match(f"^领奖 .+ [0-9]+$", command):
         moneyInPot = re.findall(f"^领奖 (.+) ([0-9]+)$", command)[0][1]
         await sendAward(self, message, db, int(moneyInPot) * 100, message.id, storage.eventRoles, command)
