5a15fed2ecee55d92103bbad1756bde7e5e430d5
Linbin <i@teenet.me>
2022-02-17 13:36:47 -0500

    Luckymoney (#69)
    * update luckymoney
    
    * make lucky money embed looks nicer

--- src/controller/routes/luckyMoney/getLuckyMoney.py
+++ src/controller/routes/luckyMoney/getLuckyMoney.py
@@ -1,4 +1,4 @@
-from discord import Client, TextChannel, Guild, Member
+from discord import Client, TextChannel, Guild, Member, Message, Embed
 from pymysql import Connection
 import src.model.luckyMoneyManagement as luckyMoneyManagement
 from src.model.userManagement import addMoneyToUser
@@ -7,6 +7,7 @@ from typing import List
 import json
 import random
 from src.utils.readConfig import getLanguageConfig, getMajorConfig, getCashFlowMsgConfig
+import embedLib.luckymoney as luckyMoneyEmbed
 
 
 
@@ -17,12 +18,9 @@ async def getLuckyMoney(self: Client, messageID: int, db: Connection, channelID:
 
     myGuild: Guild = self.guilds[0]
     user: Member = await myGuild.fetch_member(userID)
-    channels: List[TextChannel] = myGuild.text_channels
-    targetChannel: TextChannel or None = None
-    for channel in channels:
-        if channel.id == channelID:
-            targetChannel = channel
-            break
+    targetChannel: TextChannel = myGuild.get_channel(channelID)
+    message: Message = await targetChannel.fetch_message(messageID)
+    messageEmbed: Embed = message.embeds[0]
 
 
     luckyMoneyInfo = luckyMoneyManagement.getLuckyMoney(db, messageID)
@@ -30,24 +28,15 @@ async def getLuckyMoney(self: Client, messageID: int, db: Connection, channelID:
     if luckyMoneyInfo[3] == 1:
         lastOne = True
 
-    if luckyMoneyInfo is None:
-        msg = languageConfig['luckyMoney']['luckyMoneyDoesNotExist']\
-            .replace('?@user', user.display_name)
-        await targetChannel.send(msg)
+    if luckyMoneyInfo is None: # lucky money does not existed
         return
 
-    if luckyMoneyInfo[3] == 0:
-        msg = languageConfig['luckyMoney']['luckyMoneyHasAlreadyBeenTaken']\
-            .replace('?@user', user.display_name)
-        await targetChannel.send(msg)
+    if luckyMoneyInfo[3] == 0: # lucky money has already been taken
         return
 
     whoTake: dict = json.loads(luckyMoneyInfo[5])
 
     if str(userID) in whoTake:
-        msg = languageConfig['luckyMoney']['youHaveAlreadyTaken']\
-            .replace('?@user', user.display_name)
-        await targetChannel.send(msg)
         return
 
 
@@ -69,15 +58,14 @@ async def getLuckyMoney(self: Client, messageID: int, db: Connection, channelID:
         msg = languageConfig['error']['dbError']
         await targetChannel.send(msg)
         return
+
     if moneyTakeFromLuckyMoney == 0:
-        msg = languageConfig['luckyMoney']['soSorryThatYouHaveTake0'] \
-            .replace('?@user', user.display_name)
-        await targetChannel.send(msg)
+        luckyMoneyEmbed.newPersonWithZero(messageEmbed, user.display_name)
+        await message.edit(embed=messageEmbed)
+
     else:
-        msg = languageConfig['luckyMoney']['congratulationYouHaveTake'] \
-            .replace('?@user', user.display_name) \
-            .replace('?@money', str(moneyTakeFromLuckyMoney / 100))
-        await targetChannel.send(msg)
+        luckyMoneyEmbed.newPerson(messageEmbed, user.display_name, str(moneyTakeFromLuckyMoney / 100))
+        await message.edit(embed=messageEmbed)
 
     if lastOne:
         sender: Member = await myGuild.fetch_member(luckyMoneyInfo[1])
@@ -93,13 +81,8 @@ async def getLuckyMoney(self: Client, messageID: int, db: Connection, channelID:
                 highestTake = whoTake[who]
 
         mostLuckyMember = await myGuild.fetch_member(mostLucky)
-        msg = languageConfig['luckyMoney']['noLuckMoneyLeft']\
-            .replace('?@sender', sender.display_name)
-        await targetChannel.send(msg)
         if mostLuckyMember is None:
             return
-        msg = languageConfig['luckyMoney']['theMostLuckyGuy'] \
-            .replace('?@user', mostLuckyMember.display_name) \
-            .replace('?@money', str(highestTake / 100))
-        await targetChannel.send(msg)
+        luckyMoneyEmbed.addBestLuck(messageEmbed, mostLuckyMember.display_name, str(highestTake / 100))
+        await message.edit(embed=messageEmbed)
 
