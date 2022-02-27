64e3de4547f4b6ac58508256bbcd4b99a2fd3830
Pluto Zhang <lionelzhang99@gmail.com>
2022-02-26 22:16:15 -0500

    添加了embed send box发送功能

--- src/controller/routes/generateRandom.py
+++ src/controller/routes/generateRandom.py
@@ -6,7 +6,7 @@ from discord import Message, Member
 from pymysql import Connection
 from pathlib import Path
 from src.utils.readConfig import getLanguageConfig
-
+import embedLib.randomNumber
 languageConfig = getLanguageConfig()
 
 
@@ -26,19 +26,26 @@ async def generateRandom(message: Message, db: Connection):
     if userInfo is None:
         logger.error(f"User {user.id} check balance failed")
         messageSendBack: str = str(languageConfig['error']["dbError"])
+        await message.channel.send(messageSendBack)
     elif ranges <= 0 or ranges > 100:
         logger.error(f"User has entered an illegal range value")
         messageSendBack: str = str(languageConfig['randomNumber']['rangesError'])
+        await message.channel.send(messageSendBack)
     elif repeatTime <= 0:
         logger.error(f"User has entered an illegal repeat time value")
         messageSendBack: str = str(languageConfig['randomNumber']['repeatTimeError'])
-    elif repeatTime > ranges:
-        logger.error(f"Because this random system is non-repeatable, repeat time have to bigger than ranges")
+        await message.channel.send(messageSendBack)
+    elif repeatTime >= ranges:
+        logger.error(f"Because this random system is non-repeatable, repeat time have to bigger or equal than ranges")
         messageSendBack: str = str(languageConfig['randomNumber']['rangesAndRepeatTimeError'])
+        await message.channel.send(messageSendBack)
     else:
-        displayFormat: str = str(languageConfig['randomNumber']['randomNumberFormat'])
-        displayRandom: str = str(random.sample(range(1, ranges), repeatTime))
-        messageSendBack = displayFormat \
-            .replace("?@user", f"{user.display_name}") \
-            .replace("?@displayRandomNumber", f"{displayRandom}")
-    await message.channel.send(messageSendBack)
+        randomNumber : str = str(random.sample(range(1, ranges), repeatTime))
+        embedMsg = embedLib.randomNumber.getEmbed(user.display_name, randomNumber)
+        await message.channel.send(embed=embedMsg)
+        # displayFormat: str = str(languageConfig['randomNumber']['randomNumberFormat'])
+
+        # messageSendBack = displayFormat \
+        #     .replace("?@user", f"{user.display_name}") \
+        #     .replace("?@displayRandomNumber", f"{displayRandom}")
+        # displayRandom: str = str(random.sample(range(1, ranges), repeatTime))
