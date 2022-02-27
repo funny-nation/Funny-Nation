b7376bb4492465203696908a3ad0f74e86aecf7a
Yinliang Lyu <ylyu2@binghamton.edu>
2022-02-27 02:28:20 -0500

    finished help system

--- src/controller/routes/help/getHelpForGift.py
+++ src/controller/routes/help/getHelpForGift.py
@@ -1,8 +1,17 @@
 from discord import Client, Message
 from src.utils.readConfig import getLanguageConfig
+from src.utils.readConfig import getGiftConfig
 import embedLib.help.getHelp3 as help
 languageConfig = getLanguageConfig()
+giftConfig = getGiftConfig()
 
 async def getHelpForGift(self: Client, message: Message):
-    embed = help.getEmbed()
+    sections: str = giftConfig.sections()
+    description: str = ""
+
+    for i in range(0, len(giftConfig.sections())):
+        giftName: str = sections[i]
+        giftMoney = giftConfig[giftName]['amount']
+        description += f"{giftName}: {giftMoney}元\n"
+    embed = help.getEmbed(description)
     await message.channel.send(embed=embed)
--- src/controller/routes/help/getHelpForVIP.py
+++ src/controller/routes/help/getHelpForVIP.py
@@ -1,8 +1,16 @@
 from discord import Client, Message
-from src.utils.readConfig import getLanguageConfig
+from src.utils.readConfig import getVipTagsConfig
 import embedLib.help.getHelp4 as help
-languageConfig = getLanguageConfig()
+VIPConfig = getVipTagsConfig()
 
 async def getHelpForVIP(self: Client, message: Message):
-    embed = help.getEmbed()
+    sections: str = VIPConfig.sections()
+    field = ""
+    for i in range(0, len(VIPConfig.sections())):
+        vipNnumber: str = sections[i]
+        VIPName = VIPConfig[vipNnumber]['name']
+        giftMoney = VIPConfig[vipNnumber]['price']
+        benifitNumber = VIPConfig[vipNnumber]['benefit']
+        field += f"{VIPName}-{giftMoney}-{benifitNumber}\n"
+    embed = help.getEmbed(field)
     await message.channel.send(embed=embed)
