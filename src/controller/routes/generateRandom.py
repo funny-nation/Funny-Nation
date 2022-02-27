80c8c8a9178b0bef8e230fae73abd3ce60ae4b84
Pluto Zhang <lionelzhang99@gmail.com>
2022-02-27 01:26:31 -0500

    修复了关于代码规范的问题

--- src/controller/routes/generateRandom.py
+++ src/controller/routes/generateRandom.py
@@ -27,18 +27,21 @@ async def generateRandom(message: Message, db: Connection):
         logger.error(f"User {user.id} check balance failed")
         messageSendBack: str = str(languageConfig['error']["dbError"])
         await message.channel.send(messageSendBack)
-    elif ranges <= 0 or ranges > 100:
+    if ranges <= 0 or ranges > 100:
         logger.error(f"User has entered an illegal range value")
         messageSendBack: str = str(languageConfig['randomNumber']['rangesError'])
         await message.channel.send(messageSendBack)
-    elif repeatTime <= 0:
+        return
+    if repeatTime <= 0:
         logger.error(f"User has entered an illegal repeat time value")
         messageSendBack: str = str(languageConfig['randomNumber']['repeatTimeError'])
         await message.channel.send(messageSendBack)
-    elif repeatTime >= ranges:
+        return
+    if repeatTime >= ranges:
         logger.error(f"Because this random system is non-repeatable, repeat time have to bigger or equal than ranges")
         messageSendBack: str = str(languageConfig['randomNumber']['rangesAndRepeatTimeError'])
         await message.channel.send(messageSendBack)
+        return
     else:
         randomNumber : str = str(random.sample(range(1, ranges), repeatTime))
         embedMsg = embedLib.randomNumber.getEmbed(user.display_name, randomNumber)
