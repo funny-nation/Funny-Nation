62197571c53de26fbf2885a88e2f767e39d83e8a
IvanTse <sdyaivan@icloud.com>
2022-07-25 23:14:23 -0400

    Transferring coins multi language support -- again. Idk why last commit brought some problems

--- src/features/transfer-coin/index.ts
+++ src/features/transfer-coin/index.ts
@@ -7,47 +7,62 @@ import getDbGuild from '../../models/db-guild/get-db-guild'
 import getLanguage from '../../language/get-language'
 import logger from '../../logger'
 client.on('interactionCreate', async (interaction:Interaction) => {
+  // interactionCreate filtering
   if (
     !interaction.isCommand() ||
     interaction.guild === null ||
     interaction.member === null ||
     !(interaction.member instanceof GuildMember)
   ) return
-  if (!(interaction.member instanceof GuildMember)) return
 
-  const payee = interaction.options.getUser('payee')
-  const coin = interaction.options.getInteger('coin')
-  const detailMsg = interaction.options.getString('detail')
+  // Fetching user's language
+  const dbGuild = await getDbGuild(interaction.guild.id)
+  const language = getLanguage(dbGuild.languageInGuild).transferCoin
+  if (interaction.commandName !== language.transferCommand) return
 
-  if (payee === null || coin === null) return
+  // Fetching command arguments
+  const payee = interaction.options.getUser(language.payee)
+  const coin = interaction.options.getInteger(language.coin)
+  const detailMsg = interaction.options.getString(language.detail)
 
+  // Compulsory arguments checking
+  if (!(payee && coin)) {
+    logger.info(`Null payee or coin exception\n${interaction.options}`)
+    return
+  }
+
+  // Extracting guild info
   const payer :GuildMember = interaction.member
   const guild: Guild = interaction.guild
 
+  // Fetching payer's and payee's info from database
   const dbMember = await getDbMember(payer.id, guild.id)
   const payeeDbMember = await getDbMember(payee.id, guild.id)
 
+  // Coin balance checking
   if (coin > dbMember.coinBalanceInGuild) {
-    await interaction.reply('你没钱')
+    await interaction.reply(language.insufficientBalance)
     return
   }
+  // Operating in database
   await dbMember.reduceCoins(coin)
   await payeeDbMember.addCoins(coin)
-  const transactionID = randomUUID()
+  const transactionID = randomUUID() // Generate a shared UUID for matching a pair of DR & CR records.
   await addDbCoinTransfer(payee.id, guild.id, coin, transactionID, detailMsg || '', 'transferOut')
   await addDbCoinTransfer(payer.id, guild.id, -coin, transactionID, detailMsg || '', 'transferIn')
+  // Interaction replying successful transferring message
   if (detailMsg) {
     await interaction.reply({
-      content: `Transfer complete! <@${payee.id}> You have received the coins. `,
+      content: language.transferCompleteMsg(payee.id, coin),
       embeds: [
         new MessageEmbed()
           .setTitle(detailMsg)
-          .setDescription('Sender has leave a message to you')
+          .setDescription(language.senderLeavingMsgInfo)
       ]
     })
   } else {
     await interaction.reply({
-      content: `Transfer complete! <@${payee.id}> You have received the coins. `
+      content: language.transferCompleteMsg(payee.id, coin)
     })
   }
 })
