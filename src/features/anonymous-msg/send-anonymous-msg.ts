637a92badc68cab3729f21dc8d2e874eedd305b9
Linbin <i@teenet.me>
2022-08-15 20:53:34 -0400

    Adding Try Catch for anonymous message (#160)
    * Leader board change inline
    
    * Add try catch for anonymous msg
    
    * Daily check in feature
    
    * newCommands now support language and guild ID parameters
    
    * Document update
    
    * Disable Gift feature
    
    * Refactor Anonymous feature
    
    * When user in voice, whatever how long he stay, he will still earn coins and exp
    
    * Fix super undefined issue
    
    * Display ranking in view profile
    
    * Add Chinese Curse words
    
    * Rename transfer coins desc
    
    * Enable read .env
    
    * Formatting anonymous message
    
    * Catch error for anonymous message

--- src/features/anonymous-msg/send-anonymous-msg.ts
+++ src/features/anonymous-msg/send-anonymous-msg.ts
@@ -3,41 +3,47 @@ import { Interaction, MessageEmbed } from 'discord.js'
 import { getDbGuild, getDbUser } from '../../models'
 import { getLanguage } from '../../language'
 import { names, uniqueNamesGenerator } from 'unique-names-generator'
+import { logger } from '../../logger'
 
 client.on('interactionCreate', async (interaction: Interaction) => {
-  if (!interaction.isCommand()) return
-  if (!interaction.guild) return
-  if (!interaction.user) return
-  if (!interaction.channel) return
+  try {
+    if (!interaction.isCommand()) return
+    if (!interaction.guild) return
+    if (!interaction.user) return
+    if (!interaction.channel) return
 
-  if (interaction.commandName !== 'anonymous') return
-  const dbGuild = await getDbGuild(interaction.guild.id)
-  const language = getLanguage(dbGuild.languageInGuild)
-  if (interaction.options.getSubcommand() !== language.anonymousMsg.commands.send.name) return
-  const anonymousMsg = interaction.options.getString(language.anonymousMsg.commands.send.MsgOptionName)
-  if (!anonymousMsg) return
+    if (interaction.commandName !== 'anonymous') return
+    const dbGuild = await getDbGuild(interaction.guild.id)
+    const language = getLanguage(dbGuild.languageInGuild)
+    if (interaction.options.getSubcommand() !== language.anonymousMsg.commands.send.name) return
+    const anonymousMsg = interaction.options.getString(language.anonymousMsg.commands.send.MsgOptionName)
+    if (!anonymousMsg) return
 
-  const dbUser = await getDbUser(interaction.user.id)
-  if (!dbUser.anonymousNickName) {
-    const newNickName = uniqueNamesGenerator({
-      dictionaries: [names]
-    })
-    await dbUser.setAnonymousNickName(newNickName)
-  }
-  await interaction.deferReply({ ephemeral: true })
-  await interaction.editReply('^ ^')
-  const mention = interaction.options.getUser(language.anonymousMsg.commands.send.UserOptionName)
-  const embed = new MessageEmbed()
-    .setDescription(`${dbUser.anonymousNickName}🫣:\n${anonymousMsg}`)
-    .setColor('#FF99CC')
-  if (mention) {
-    await interaction.channel.send({
-      content: mention.toString(),
-      embeds: [embed]
-    })
-  } else {
-    await interaction.channel.send({
-      embeds: [embed]
-    })
+    const dbUser = await getDbUser(interaction.user.id)
+    if (!dbUser.anonymousNickName) {
+      const newNickName = uniqueNamesGenerator({
+        dictionaries: [names]
+      })
+      await dbUser.setAnonymousNickName(newNickName)
+    }
+    await interaction.deferReply({ ephemeral: true })
+    await interaction.editReply('^ ^')
+    const mention = interaction.options.getUser(language.anonymousMsg.commands.send.UserOptionName)
+    const embed = new MessageEmbed()
+      .setDescription(`${dbUser.anonymousNickName}🫣:\n${anonymousMsg}`)
+      .setColor('#FF99CC')
+    if (mention) {
+      await interaction.channel.send({
+        content: mention.toString(),
+        embeds: [embed]
+      })
+    } else {
+      await interaction.channel.send({
+        embeds: [embed]
+      })
+    }
+  } catch (e) {
+    console.log(e)
+    logger.error('Error when sending anonymous message')
   }
 })
--- src/features/anonymous-msg/set-nick-name.ts
+++ src/features/anonymous-msg/set-nick-name.ts
@@ -2,21 +2,27 @@ import { client } from '../../client'
 import { Interaction } from 'discord.js'
 import { getDbGuild, getDbUser } from '../../models'
 import { getLanguage } from '../../language'
+import { logger } from '../../logger'
 
 client.on('interactionCreate', async (interaction: Interaction) => {
-  if (!interaction.isCommand()) return
-  if (!interaction.guild) return
-  if (!interaction.user) return
+  try {
+    if (!interaction.isCommand()) return
+    if (!interaction.guild) return
+    if (!interaction.user) return
 
-  if (interaction.commandName !== 'anonymous') return
-  const dbGuild = await getDbGuild(interaction.guild.id)
-  const language = getLanguage(dbGuild.languageInGuild)
-  if (interaction.options.getSubcommand() !== language.anonymousMsg.commands.setName.name) return
-  const nickName = interaction.options.getString(language.anonymousMsg.commands.setName.optionName)
-  if (!nickName) return
+    if (interaction.commandName !== 'anonymous') return
+    const dbGuild = await getDbGuild(interaction.guild.id)
+    const language = getLanguage(dbGuild.languageInGuild)
+    if (interaction.options.getSubcommand() !== language.anonymousMsg.commands.setName.name) return
+    const nickName = interaction.options.getString(language.anonymousMsg.commands.setName.optionName)
+    if (!nickName) return
 
-  const dbUser = await getDbUser(interaction.user.id)
-  await dbUser.setAnonymousNickName(nickName)
-  await interaction.deferReply({ ephemeral: true })
-  await interaction.editReply(`${language.anonymousMsg.yourNewNameIs}\`${nickName}\``)
+    const dbUser = await getDbUser(interaction.user.id)
+    await dbUser.setAnonymousNickName(nickName)
+    await interaction.deferReply({ ephemeral: true })
+    await interaction.editReply(`${language.anonymousMsg.yourNewNameIs}\`${nickName}\``)
+  } catch (e) {
+    logger.error('Error when user setting the anonymous message nickname')
+    console.log(e)
+  }
 })
